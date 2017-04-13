1be030a7288258af3ae5e7fb75b3729f06bb8af5
hendry19901990 <elnegroevaristo@gmail.com>
2017-04-12 22:45:23 -0430

    Estructura Principal

---
+++ src/main/java/com/eth/smart/contratos/App.java
@@ -0,0 +1,13 @@
+package com.eth.smart.contratos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
